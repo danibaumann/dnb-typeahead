e2473db1e850878b97e5c101c1974f2692a696bf
Dani Baumann <13361949+danibaumann@users.noreply.github.com>
2020-05-14 14:40:58 +0200

    0.0.4

--- assets/js/dnb.js
+++ assets/js/dnb.js
@@ -1,33 +1,40 @@
 class DnB {
   constructor() {
-    this.searchURL = 'https://api.dev.screenstyle.ch/v1/search';
-    this.matchURL = 'https://api.dev.screenstyle.ch/v1/match'
+    this.yourHost = 'localhost:3000';
+    this.searchURL = `${this.yourhost}/v1/search`;
+    this.matchURL = `${this.yourhost}/v1/match`;
   }
 
   async getCompany(searchText) {
     const headers = new Headers({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
-    const companyResponse = await fetch(`${this.searchURL}/typeahead?searchTerm=${searchText}`, {
-      headers
-    });
+    const companyResponse = await fetch(
+      `${this.searchURL}/typeahead?searchTerm=${searchText}`,
+      {
+        headers,
+      }
+    );
 
     const data = await companyResponse.json();
 
-    return data
+    return data;
   }
 
   // fetch company details based on duns number
   async getCompanyDetailsForDuns(duns) {
-    console.log(`will fetch company details for DUNS ${duns}`)
+    console.log(`will fetch company details for DUNS ${duns}`);
     const headers = new Headers({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
-    const companyDetails = await fetch(`${this.matchURL}/cleanseMatch?duns=${duns}`, {
-      headers
-    });
+    const companyDetails = await fetch(
+      `${this.matchURL}/cleanseMatch?duns=${duns}`,
+      {
+        headers,
+      }
+    );
     const data = await companyDetails.json();
 
-    return data
+    return data;
   }
 }
